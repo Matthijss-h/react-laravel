0a85601dd736e8ff5bfcdd465bde14a60a40e9d5
Aech <vincentbakker.08@gmail.com>
2026-02-02 12:21:09 +0100

    Responsive home with text css

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -16,11 +16,10 @@ export default function Welcome({
         <>
             <Head title="Welcome">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
             </Head>
                <header className="fixed top-0 w-full z-50 bg-white/80">
                  <nav className=" relative flex items-center justify-between px-6 py-3 max-w mx-auto h-16">
-    
                 <Link href={home()} className="absolute left-5 top-1 -translate-y-0.5">
       <img src={logo} alt="Your Company" className="h-40 w-auto" />
       </Link>
@@ -51,25 +50,33 @@ export default function Welcome({
   </nav>
 </header>
             <div className="flex w-full items-center min-h-screen bg-background justify-center">
-                <main className="flex w-full justify-center lg:max-w-4xl lg:flex-row">               
+                <main className="flex w-full">               
                     <div className="flex flex-col lg:flex-row gap-8 items-center justify-center w-full">
-                      <img src={bubbles} alt="Bubbles" className="h-64 w-auto" />
-                      <div className="flex flex-col gap-4 bg-pink-700 p-8 rounded-lg h-64 w-80 justify-between">
-                        <div>
-                          <h1 className="text-2xl font-bold text-white">Grolloo Network</h1>
-                          <p className="text-white/90 mt-2">Verken alle networken van Grolloo!</p>
-                        </div>
-                        <AiFillCaretDown className="h-10 w-8 text-white" />
-                        
-                        {/* <Link
+                      <img src={bubbles} alt="Bubbles" className="h-90 w-auto" />
+                        <div className="flex flex-col gap-4 bg-[#33341C] p-6 sm:p-8 rounded-lg h-auto sm:h-64 flex-1 justify-center ml-auto">
+                          <div>
+                          <h1 className="text-3xl font-black text-[#E1A07F]">Grolloo Network</h1>
+                          <p className="text-2xl font-semibold text-white mt-5">Verken alle networken van Grolloo!</p>
+                          </div>
+                          <AiFillCaretDown className="h-8 sm:h-10 w-6 sm:w-8 text-white" />
+                          
+                          {/* <Link
                           href={network()}
                           className="inline-block rounded-sm border border-white px-5 py-1.5 text-sm text-white hover:bg-white/10 transition w-fit">
                           Open Network
-                        </Link> */}
-                      </div>
-                    </div>
+                          </Link> */}
+                          
+                        </div>
+                    </div>                         
                 </main>
             </div>
+            <section className="w-full text-start ml-30">
+              <h1 className="text-[#33341C] text-3xl font-bold">Grolloo Networken</h1>
+            
+            {/* Grid with network cards in here */}
+            </section>
+
         </>
+        
     );
 }
